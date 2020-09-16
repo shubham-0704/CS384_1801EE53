953e8304fdac2ae4e1c7dc850775a3d76153a88f
Shubham kumar <shubhamrxl074@gmail.com>
2020-09-16 17:37:48 +0530

    basic calculator

---
+++ Assignment1/test_cases_tutorial01.py
@@ -0,0 +1,28 @@
+import tutorial01 as A1
+
+actual_answers = [9, 12,28,7]
+student_answers = []
+
+test_case_1 = A1.add(4, 5)
+student_answers.append(test_case_1)
+
+test_case_2 = A1.subtract(14, 2)
+student_answers.append(test_case_2)
+
+test_case_3 = A1.multiply(14, 2)
+student_answers.append(test_case_3)
+
+test_case_4 = A1.divide(14, 2)
+student_answers.append(test_case_4)
+
+print(actual_answers)
+print(student_answers)
+
+total_test_cases = len(actual_answers)
+count_of_correct_test_cases = 0
+
+for x, y in zip(actual_answers, student_answers):
+    if x == y:
+        count_of_correct_test_cases += 1
+
+print(f"Test Cases Passed = '{count_of_correct_test_cases}'  / '{total_test_cases}'")
---
+++ Assignment1/tutorial01.py
@@ -0,0 +1,22 @@
+# Function to add two numbers 
+def add(num1, num2): 
+	addition = num1 + num2
+	return addition
+
+# Function to subtract two numbers 
+def subtract(num1, num2): 
+	subtraction = num1 - num2
+	return subtraction
+
+# Function to multiply two numbers 
+def multiply(num1, num2): 
+	#Multiplication Logic 
+	multiplication=num1*num2
+	return multiplication
+
+# Function to divide two numbers 
+def divide(num1, num2): 
+	#DivisionLogic 
+	division =num1/num2
+	return division
+	
