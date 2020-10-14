2e283dd90bb51dd40d1fb25a023262ff45d597df
Shubham kumar <shubhamrxl074@gmail.com>
2020-10-14 14:53:39 +0530

    initialized assignment3 folder in repo

---
+++ Assignment3/tutorial03.py
@@ -0,0 +1,42 @@
+import csv
+
+
+def course():
+    # Read csv and process
+    pass
+
+
+def country():
+    # Read csv and process
+    pass
+
+
+def email_domain_extract():
+    # Read csv and process
+    pass
+
+
+def gender():
+    # Read csv and process
+    pass
+
+
+def dob():
+    # Read csv and process
+    pass
+
+
+def state():
+    # Read csv and process
+    pass
+
+
+def blood_group():
+    # Read csv and process
+    pass
+
+
+# Create the new file here and also sort it in this function only.
+def new_file_sort():
+    # Read csv and process
+    pass
