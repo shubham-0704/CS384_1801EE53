6ed457fb2d29a81ffb55acc767aa90fae8ac2eb2
Shubham kumar <shubhamrxl074@gmail.com>
2020-10-14 16:18:35 +0530

    added gender function to divide data in male and female

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -1,4 +1,6 @@
 import csv
+import re
+import os
 
 
 def course():
@@ -17,8 +19,32 @@ def email_domain_extract():
 
 
 def gender():
-    # Read csv and process
-    pass
+    path=os.getcwd()
+    path=path+r"/analytics/gender"
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    file=open('studentinfo_cs384.csv','r')
+    m=open(path+r"/male.csv",'w',newline="")
+    f=open(path+r"/female.csv",'w',newline="")
+    with file:        
+            reader=csv.DictReader(file)
+            fieldname=reader.fieldnames
+            with m:
+                writer=csv.DictWriter(m,fieldnames=fieldname)
+                writer.writeheader()
+                for row in reader:
+                         if(row["gender"].lower()=="male"):       
+                                writer.writerow(row)
+    file=open('studentinfo_cs384.csv','r')
+    with file:
+            reader=csv.DictReader(file)
+            fieldname=reader.fieldnames       
+            with f:
+                writer=csv.DictWriter(f,fieldnames=fieldname)
+                writer.writeheader()
+                for row1 in reader:
+                         if(row1["gender"].lower()=="female"):       
+                                writer.writerow(row1)
 
 
 def dob():
