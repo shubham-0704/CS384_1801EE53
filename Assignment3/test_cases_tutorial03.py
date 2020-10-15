666bc5980037b36fd04b43c82ac54ddb2b54a871
Shubham kumar <shubhamrxl074@gmail.com>
2020-10-15 18:49:13 +0530

    added test_case_tutorial03.py file for testing

---
+++ Assignment3/test_cases_tutorial03.py
@@ -0,0 +1,12 @@
+import tutorial03 as A3
+import csv
+
+A3.del_create_analytics_folder()
+A3.course()
+A3.country()
+A3.email_domain_extract()
+A3.gender()
+A3.dob()
+A3.state()
+A3.blood_group()
+A3.new_file_sort()
