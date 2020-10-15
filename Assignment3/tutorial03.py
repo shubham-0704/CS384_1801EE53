d41cf8f03e3b5487129f6be9c07c921677218fa9
Shubham kumar <shubhamrxl074@gmail.com>
2020-10-15 16:07:58 +0530

    added course function for arranging students with their branch and degree

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -4,8 +4,66 @@ import os
 
 
 def course():
-    # Read csv and process
-    pass
+    path=os.getcwd()
+    # path=path+r"/analytics/course"
+    path=os.path.join(path,"analytics","course")
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    file=open('studentinfo_cs384.csv','r')
+    fieldname=""
+    reader=csv.DictReader(file)
+    fieldname=reader.fieldnames
+    pattern=re.compile(r"[0-9]{4}[a-zA-Z]{2}[0-9]{2}")
+    course_name={"01":"btech","11":"mtech","12":"msc","21":"phd"}
+    course_num=["01","11","12","21"]
+    for line in reader:
+        if re.fullmatch(pattern,line["id"]):
+            branch=line["id"][4:6].lower()
+            year=line["id"][0:2].lower()
+            course=line["id"][2:4].lower()
+            # course=course_name["%s"%course]
+            if course in course_num:
+                course=course_name[course]
+
+                # if not os.path.isdir(path+r"/%s"%branch):
+                #     os.makedirs(path+r"/%s"%branch)
+                if not os.path.isdir(os.path.join(path,branch)):
+                    os.makedirs(os.path.join(path,branch))
+
+                # if not os.path.isdir(path+r"/%s/%s"%(branch,course)):
+                #     os.makedirs(path+r"/%s/%s"%(branch,course))
+                if not os.path.isdir(os.path.join(path,branch,course)):
+                    os.makedirs(os.path.join(path,branch,course))
+                flag=0
+                file_name=year+"_"+branch+"_"+course+".csv"
+                # if not os.path.isfile(path+r"/%s/%s/%s"%(branch,course,file_name)):flag=1
+                if not os.path.isfile(os.path.join(path,branch,course,file_name)):flag=1
+                # f=open(path+r"/%s/%s/%s"%(branch,course,file_name),'a+',newline="")
+                f=open(os.path.join(path,branch,course,file_name),'a+',newline="")
+                writer=csv.DictWriter(f,fieldnames=fieldname)
+                if flag:writer.writeheader()
+                writer.writerow(line)
+            else:
+                i="misc"+".csv"
+                flag=0
+
+                if not os.path.isfile(os.path.join(path,i)):flag=1
+                f=open(os.path.join(path,i),'a+',newline="")
+                
+                writer=csv.DictWriter(f,fieldnames=fieldname)
+                if flag:writer.writeheader()
+                writer.writerow(line)
+
+        else:
+            i="misc"+".csv"
+            flag=0
+
+            if not os.path.isfile(os.path.join(path,i)):flag=1
+            f=open(os.path.join(path,i),'a+',newline="")
+            
+            writer=csv.DictWriter(f,fieldnames=fieldname)
+            if flag:writer.writeheader()
+            writer.writerow(line)
 
 
 def country():
