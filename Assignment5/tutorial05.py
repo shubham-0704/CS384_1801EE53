7a50421fc71588795f9ef013000f799992a0c64f
Shubham kumar <shubhamrxl074@gmail.com>
2020-11-15 18:16:29 +0530

    added codes for rename of sherlock

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -41,8 +41,24 @@ def rename_Game_of_Thrones(folder_name):
     
 
 def rename_Sherlock(folder_name):
-    # rename Logic
-    pass 
+    # rename Logic 
+    path=os.path.join(os.getcwd(),"subtitles",folder_name)
+    title=input("Main Title of the Web Series: ")
+    sea_pad=int(input("Season Number Padding: "))
+    epi_pad=int(input("Episode Number Padding: "))
+
+    for name in os.listdir(path):
+
+            src=os.path.join(path,name)
+            file_type=name.split(".")[-1]
+            sea_num=str(int(re.search(r'S[0-9]{2}',name).group()[1:]))
+            ep_num=str(int(re.search(r'E[0-9]{2}',name).group()[1:]))
+            f_name=title+" - "+"Season "+ sea_num.zfill(sea_pad)+" Episode "+ep_num.zfill(epi_pad)+"."+file_type
+            des=os.path.join(path,f_name)
+            if not os.path.isfile(des):
+                os.rename(src,des)
+            else :
+                os.remove(src) 
     
 
 def rename_Suits(folder_name):
