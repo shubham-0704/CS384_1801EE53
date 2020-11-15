601095ab3a8589d8780a15c9b6dc18d6c8d5257f
Shubham kumar <shubhamrxl074@gmail.com>
2020-11-15 18:08:53 +0530

    added base file for assingnment5

---
+++ Assignment5/tutorial05.py
@@ -0,0 +1,20 @@
+
+def rename_FIR(folder_name):
+    # rename Logic 
+    
+
+def rename_Game_of_Thrones(folder_name):
+    # rename Logic 
+    
+
+def rename_Sherlock(folder_name):
+    # rename Logic 
+    
+
+def rename_Suits(folder_name):
+    # rename Logic 
+    
+
+def rename_How_I_Met_Your_Mother(folder_name):
+    # rename Logic 
+    
