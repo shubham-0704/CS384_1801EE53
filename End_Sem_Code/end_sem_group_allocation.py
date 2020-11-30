b88922c3b2d86121197a40b8883a89f04dba1a84
Shubham kumar <shubhamrxl074@gmail.com>
2020-11-30 11:05:06 +0530

    added master data and bas code file

---
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -0,0 +1,8 @@
+
+def group_allocation(filename, number_of_groups):
+    # Entire Logic 
+	# You can add more functions, but in the test case, we will only call the group_allocation() method,
+
+filename = "Btech_2020_master_data.csv"
+number_of_groups = 12 
+group_allocation(filename, number_of_groups)
