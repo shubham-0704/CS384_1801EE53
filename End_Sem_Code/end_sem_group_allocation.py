0cc9d064955b094fb8fd75ebeca00851b074766a
Shubham kumar <shubhamrxl074@gmail.com>
2020-11-30 11:33:07 +0530

    added code to make stats file of groups

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -33,6 +33,37 @@ def group_allocation(filename, number_of_groups):
         df_str=df_str.append({"BRANCH_CODE":i,"STRENGTH":df.shape[0]},ignore_index=True)
     df_str.sort_values(["STRENGTH","BRANCH_CODE"], ascending=[False, True], inplace=True)
     df_str.to_csv(f"groups/branch_strength.csv",index=False)
+
+
+    # making groups
+    nog=number_of_groups
+    col=list(df_str["BRANCH_CODE"])
+    col.insert(0,"total")
+    col.insert(0,"group")
+    bran_str = dict.fromkeys(list(df_str["BRANCH_CODE"]),0)
+    list_grp=list(range(nog))
+    list_grp=[bran_str.copy() for x in list_grp]
+    df=pd.DataFrame(columns=col)
+
+    for i,x in zip(df_str["BRANCH_CODE"],df_str["STRENGTH"]):
+        for n in range(nog):
+            list_grp[n][i]=x//nog
+        bran_str[i]=(x-((x//nog)*nog))
+    a=0
+    for i,x in bran_str.items():
+        # print(i,x)
+        while(x):
+            list_grp[a][i]+=1
+            x-=1
+            a+=1
+            a=a%nog
+    for i in range(nog):
+        list_grp[i]["total"]=sum(list_grp[i].values())
+        list_grp[i]["group"]="Group_G"+str(i+1).zfill(2)+".csv"
+
+
+    df=df.append(list_grp)
+    df.to_csv(f"groups/stats_grouping.csv",index=False)
 filename = "Btech_2020_master_data.csv"
 number_of_groups = 12 
 group_allocation(filename, number_of_groups)
