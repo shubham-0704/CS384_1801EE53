0dbab6c26baa06675628efb615d1f0df97861197
Shubham kumar <shubhamrxl074@gmail.com>
2020-11-30 11:07:58 +0530

    added code for making groups folder

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -1,7 +1,20 @@
+import os
+import shutil
+import pandas as pd 
 
 def group_allocation(filename, number_of_groups):
     # Entire Logic 
-	# You can add more functions, but in the test case, we will only call the group_allocation() method,
+    #code to remove previous files if present
+    path=os.getcwd()
+    path=os.path.join(path,"groups")
+    if  os.path.isdir(path):
+        shutil.rmtree(path)
+
+    # make groups folder
+    path=os.getcwd()
+    path=os.path.join(path,"groups")
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
 filename = "Btech_2020_master_data.csv"
 number_of_groups = 12 
