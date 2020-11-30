a6c922322b8ebe6158551f0a2138b4bd100c083c
Shubham kumar <shubhamrxl074@gmail.com>
2020-11-30 11:14:13 +0530

    added code to read master data and groupby branch

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -15,6 +15,13 @@ def group_allocation(filename, number_of_groups):
     path=os.path.join(path,"groups")
     if not os.path.isdir(path):
         os.mkdir(path)
+    
+    # file opening and proccesing
+    data=pd.read_csv(filename)
+    data["branch"]=[ x[4:6] for x in data["Roll"]]
+    branch=data["branch"].unique()
+    # print(branch)
+    data=data.groupby("branch")
 
 filename = "Btech_2020_master_data.csv"
 number_of_groups = 12 
