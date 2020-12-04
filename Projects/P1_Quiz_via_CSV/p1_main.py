9777db6af63d7e65acc6cf08333840256ef19f22
Shubham kumar <shubhamrxl074@gmail.com>
2020-12-04 16:43:50 +0530

    added quiz function to take quiz and calculate score and all quiz related functions

--- Projects/P1_Quiz_via_CSV/p1_main.py
+++ Projects/P1_Quiz_via_CSV/p1_main.py
@@ -59,6 +59,329 @@ def next():
     regbtn.pack(pady=10,padx=10)
 
 
+
+
+def sel_quiz(name,roll):
+    print(roll,type(roll))
+    n=tk.Label(root,text=f"Name : {name}          Roll : {roll}",font=" 20 ",bg="#ADD8E6")
+    n.place(relx=.5,rely=0.1,relheight=.09,relwidth=.9,anchor="center")
+    heading=Label(root,text="Which quiz would you like to take??",font=" 25 ",padx=10,pady=10)
+    heading.place(relx=0.5,rely=0.2,anchor="center")
+    empty_l=[]
+    for i in range(len(result)):
+        button = Button(root,  height=2, width=8,bd=4,text=result[i],command=lambda i=i: read_c(result[i]))
+        button.place(relx=0.5,rely=0.4+float(i+1)/10,anchor="center")
+        empty_l.append(button)
+    # print(result)
+ 
+    def read_c(quiz):
+        for i in empty_l:
+            i.place_forget()
+
+        # reading data 
+        df=pd.read_csv("quiz_wise_questions/{}".format(quiz))
+
+        # timer function   
+        def countdown(t):
+            if t==0:
+                show_score()
+            else:
+                mins=int(t/60)
+                sec=int(t%60)
+                timer = '{:50d}:{:<50d}'.format(mins,sec)
+                myLabel1.config(text="{}".format(timer))
+                t-=1
+                myLabel1.after(1000,lambda:countdown(t))
+        time=df.columns[-1]
+        time=re.findall(r"[0-9]+",time)
+        time=int(time[0])
+        myLabel1=Label(root,text=f"{time}:00",bg="#90ee90")
+        myLabel1.place(relx=0.5,rely=0.03,anchor="center",relwidth=.9)
+        myLabel1.after(1000,lambda:countdown(time*60-1))
+
+        heading.place_forget()
+        def next_question(q,g):
+        # takes user to next question
+   
+            if g:
+                if g!=1:
+                    g.place_forget()
+                question.place(relx=0.5,rely=0.25,anchor="center")
+                correct.place(relx=0.75,rely=0.3,anchor="center")
+                negative.place(relx=0.75,rely=0.35,anchor="center")
+                comp.place(relx=0.75,rely=0.4,anchor="center")
+                option1.place(relx=0.3,rely=0.5,anchor="center")  
+                option2.place(relx=0.7,rely=0.5,anchor="center")                
+                option3.place(relx=0.3,rely=0.6,anchor="center")  
+                option4.place(relx=0.7,rely=0.6,anchor="center") 
+                next_q.place(relx=0.5,rely=0.8,anchor="center")
+                submit_b.place(relx=0.8,rely=0.8,anchor="center")
+                # number.place_forget()
+                # for k in question_nos:
+                #     k.place_forget()
+                ask.place_forget()
+                ok_button.place_forget()
+                q-=1
+            q+=1
+            if q==len(df.index):
+                show_score()
+            question.config(text="{}. {}".format(q+1,df['question'].loc[q]))
+            correct.config(text="Correct answer : {}".format(df['marks_correct_ans'].loc[q]))
+            negative.config(text="Wrong answer : {}".format(df['marks_wrong_ans'].loc[q]))
+            comp1="No"
+            if df['compulsory'].loc[q]=="n":
+                comp1="No"
+            else :
+                comp1="Yes"
+            comp.config(text="Compulsory : {}".format(comp1))
+
+            if q==len(df.index)-1:
+                next_q.place_forget()
+
+
+            option1.config(text="{}".format(df['option1'].loc[q]),bg="white",command=lambda:choose(option1,1,q))
+            option2.config(text="{}".format(df['option2'].loc[q]),bg="white",command=lambda:choose(option2,2,q))
+            option3.config(text="{}".format(df['option3'].loc[q]),bg="white",command=lambda:choose(option3,3,q))
+            option4.config(text="{}".format(df['option4'].loc[q]),bg="white",command=lambda:choose(option4,4,q))
+            next_q.config(command=lambda:next_question(q,0))
+
+        ans_dict=dict()
+        def choose(o,num,q):      
+            # changes background color of chosen option
+            ans_dict[q]=num
+            for i in list_b:
+                i.config(bg="white")
+            o.config(bg="#00ff85") 
+            pass
+
+        # attempted=[]
+        def score_calc():
+            score=0
+            correct_ch=0
+            wrong_ch=0
+            attempted=[]
+            for i in ans_dict:
+                attempted.append(i)
+                if ans_dict[i]==df['correct_option'].loc[i]:
+                    score += df['marks_correct_ans'].loc[i]
+                    correct_ch+=1
+                else:
+                    score += df['marks_wrong_ans'].loc[i]
+                    wrong_ch+=1
+            unattempted=set([i for i in range(len(df.index))])-set(attempted)
+            for k in unattempted:
+                if df['compulsory'].loc[k]=='y':
+                    score+=df['marks_wrong_ans'].loc[k]
+            return [score,correct_ch,wrong_ch]
+            
+        # question label
+        question=Label(root,text="{}. {}".format(1,df['question'].loc[0]),wraplength=400,font=" 20 ")
+        question.place(relx=0.5,rely=0.25,anchor="center")
+
+        # Correct ans Negative and Compulsory
+        correct=Label(root,text="Correct answer : +{}".format(df['marks_correct_ans'].loc[0]),wraplength=400,)
+        correct.place(relx=0.8,rely=0.3,anchor="center")
+        negative=Label(root,text="Wrong answer : {}".format(df['marks_wrong_ans'].loc[0]),wraplength=400,)
+        negative.place(relx=0.8,rely=0.35,anchor="center")
+
+        if df['compulsory'].loc[0]=="n":
+            comp1="No"
+        else :
+            comp1="Yes"
+
+        comp=Label(root,text="Compulsory : {}".format(comp1),wraplength=400,)
+        comp.place(relx=0.8,rely=0.4,anchor="center")
+        top_labels=[question,correct,negative,comp]
+
+        # option button
+        option1=Button(root,text="{}".format(df['option1'].loc[0]),command=lambda:choose(option1,1,0),font=" 9 ",bd=3)
+        option1.place(relx=0.3,rely=0.5,anchor="center")  
+        option2=Button(root,text="{}".format(df['option2'].loc[0]),command=lambda:choose(option2,2,0),font=" 9 ",bd=3)
+        option2.place(relx=0.7,rely=0.5,anchor="center")  
+        option3=Button(root,text="{}".format(df['option3'].loc[0]),command=lambda:choose(option3,3,0),font=" 9 ",bd=3)
+        option3.place(relx=0.3,rely=0.6,anchor="center")  
+        option4=Button(root,text="{}".format(df['option4'].loc[0]),command=lambda:choose(option4,4,0),font=" 9 ",bd=3)
+        option4.place(relx=0.7,rely=0.6,anchor="center")  
+        
+        list_b=[option1,option2,option3,option4]
+        
+        #next button   
+        next_q=Button(root,text="NEXT",command=lambda:next_question(0,0),font=" 10 ",bd=4,bg="#00b8ff")  
+        next_q.place(relx=0.5,rely=0.8,anchor="center")
+        
+        # Shortcut info
+        
+        export_label=Label(root,text="Press 'Ctrl' + 'Alt' + 'e' to export quiz marks to csv",font=("Nunito","8"))
+        shortcut_label=Label(root,text="Press 'Ctrl' + 'Alt' + 'g' to go to a question | Press 'Ctrl' + 'Alt' + 'u' to show unattempted question(s)\nPress 'Ctrl' + 'Alt' + 'f' to submit quiz | Press 'Ctrl' + 'Alt' + 'e' to export quiz wise marks to csv",font=("Nunito","8"))
+        shortcut_label.place(relx=0.5,rely=0.9,anchor="center")
+        export_label.place_forget()
+        # unattempted_short=Label(root,text="Press 'Ctrl' + 'Alt' + 'u'\nto show unattempted question(s)")
+        # submit_short=Label(root,text="Press 'Ctrl' + 'Alt' + 'f'\nto submit quiz")
+        # exportdata_short=Label(root,text="Press 'Ctrl' + 'Alt' + 'e'\nto export your quiz marks to csv")
+        
+        
+        
+        question_nos=[]
+        for i in range(len(df.index)):
+                question_no = Button(root,  height=1, width=4,bd=4,text=i+1,command=lambda i=i: next_question(i,1))
+                question_nos.append(question_no)
+        
+        ask=Label(root,text=f"Which question would you like to go to? \n Any number between 1 and {len(df.index)}")
+        ok_button=Button(root,text="OK",bg="#90ee90")
+        def goto(event):
+            for i in top_labels:
+                i.place_forget()
+            next_q.place_forget()
+            submit_b.place_forget()
+            myLabel1.place_forget()
+            for k in list_b:
+                k.place_forget()
+            ask.place(relx=0.5,rely=0.2,anchor="center")
+            ok_button.config(command=lambda : goto_handler(number))
+            ok_button.place(relx=0.5,rely=0.6,anchor="center")
+            number=Entry(root,width="30",bg="white")
+            number.place(relx=0.5,rely=0.5,anchor="center")
+            number.bind("<Return>",lambda no : goto_handler(number))
+            # goto_handler(number)
+            
+        def goto_handler(number):
+            list_of_questions=[i+1 for i in range(len(df.index))]
+            entry_number=number.get()
+            check=0 
+            if len(entry_number)==0:
+                next_question(0,0)
+            else :
+                for i in range(len(entry_number)):
+                    if ord(entry_number[i])>47 and ord(entry_number[i])<58:
+                        check+=1
+                if check==len(entry_number):
+                    entry_number=int(entry_number)
+                    if entry_number in list_of_questions:
+                        next_question(entry_number-1,number)
+                    else:
+                        next_question(0,0)
+                else:
+                    next_question(0,0)
+           
+                
+                
+        # goto binding
+        if len(df.index)<30:   
+            root.bind('<Control_L><Alt_L><g>',goto)
+            root.bind('<Control_L><Alt_L><G>',goto)
+            root.bind('<Control_L><Alt_R><g>',goto)
+            root.bind('<Control_L><Alt_R><G>',goto)
+            root.bind('<Control_R><Alt_L><g>',goto)
+            root.bind('<Control_R><Alt_L><G>',goto)
+            root.bind('<Control_R><Alt_R><g>',goto)
+            root.bind('<Control_R><Alt_R><G>',goto)
+        
+        # unattempted_label=Label(root,text=f"Unattempted questions :\n {unattempted}")  
+        def unattempted_questions(event):
+            unattempted=[i+1 for i in range(len(df.index))]
+            attempted=[]
+            for i in ans_dict:
+                attempted.append(i+1)
+            unattempted=set(unattempted)-set(attempted)  
+            msg.showinfo("Unattempted Questions",f"{unattempted}")
+            
+          
+        # Bindings for unattempted question  
+        root.bind('<Control_L><Alt_L><u>',unattempted_questions)
+        root.bind('<Control_L><Alt_L><U>',unattempted_questions)
+        root.bind('<Control_L><Alt_R><u>',unattempted_questions)
+        root.bind('<Control_L><Alt_R><U>',unattempted_questions)
+        root.bind('<Control_R><Alt_L><u>',unattempted_questions)
+        root.bind('<Control_R><Alt_L><U>',unattempted_questions)
+        root.bind('<Control_R><Alt_R><u>',unattempted_questions)
+        root.bind('<Control_R><Alt_R><U>',unattempted_questions)
+       
+            
+        def show_score():
+            export_label.place(relx=0.5,rely=0.9,anchor="center")
+            shortcut_label.place_forget()
+            myLabel1.place_forget()
+            for i in top_labels:
+                i.place_forget()
+            next_q.place_forget()
+            submit_b.place_forget()
+            
+            for k in list_b:
+                k.place_forget()
+            wrong=score_calc()[2]
+            correct=score_calc()[1]
+            score=int(score_calc()[0])
+            statement=Label(root,text=f"Total questions : {len(df.index)} \n Total questions attempted : {correct+wrong} \n Total correct questions : {correct} \nTotal wrong questions : {wrong} \n Your score is {score} out of {df['marks_correct_ans'].sum()}",font="13")
+            statement.place(relx=0.5,rely=0.5,anchor='center') 
+            export_response() 
+            c.execute("""
+            SELECT * FROM project1_marks
+            WHERE roll=(?) AND quiz_num= (?);
+
+            """,(roll,quiz[:-4]))
+            if len(c.fetchall())==0:    
+                c.execute("INSERT INTO project1_marks VALUES (?,?,?)",(roll,quiz[:-4],score))
+                con.commit()
+            else:
+                c.execute("""
+                UPDATE project1_marks
+                SET total_marks=(?)
+                WHERE roll=(?) AND quiz_num= (?);
+                """,(score,roll,quiz[:-4]))
+                con.commit()
+
+
+
+
+            def clear1():
+                submit_b1.place_forget()
+                statement.place_forget()
+                # statement1.place_forget()
+                # statement2.place_forget()
+                sel_quiz(name,roll)
+            submit_b1=Button(root,text="Back To Quiz selction",command=clear1,bg="#ffdbb6",font=" 9 " ,bd=3)
+            submit_b1.place(relx=0.8,rely=0.8,anchor="center") 
+
+        # submit button
+        def conf(event=""):
+            a=msg.askyesno(title="Conformation", message="Do you want to submit",)
+            if a:
+                show_score()
+            
+
+        submit_b=Button(root,text="SUBMIT",command=conf,bg="#ffdbb6",font=" 9 " ,bd=3)
+        submit_b.place(relx=0.8,rely=0.8,anchor="center")
+        
+        #submit binding
+        root.bind('<Control_L><Alt_L><f>',conf)
+        root.bind('<Control_L><Alt_L><F>',conf)
+        root.bind('<Control_R><Alt_R><f>',conf)
+        root.bind('<Control_R><Alt_R><F>',conf)
+        root.bind('<Control_L><Alt_R><f>',conf)
+        root.bind('<Control_L><Alt_R><F>',conf)
+        root.bind('<Control_R><Alt_L><f>',conf)
+        root.bind('<Control_R><Alt_L><F>',conf)
+        
+        def export_response():
+            path=os.getcwd()
+            path=os.path.join(path,"individual_responses")
+            if not os.path.isdir(path):
+                os.mkdir(path)
+            wrong=score_calc()[2]
+            correct=score_calc()[1]
+            score=score_calc()[0]
+            legend=['Correct Choice','Wrong choice','Unattempted','Marks Obtained','Total Quiz Marks']
+            total=[correct,wrong,len(df['question'])-(correct+wrong),score,df['marks_correct_ans'].sum()]
+            df['Marked Choice']=None
+            df['Total']=None
+            df['Legend']=None
+            for i in ans_dict:
+                df['Marked Choice'].loc[i]=ans_dict[i]
+            for i in range(5):
+                df['Total'].loc[i]=total[i]
+                df['Legend'].loc[i]=legend[i]
+            df.to_csv(f'individual_responses/{quiz[:-4]}_{roll}.csv',index=False)
+
 def login():
     user=a1.get(),
     password=hash_password(b1)
